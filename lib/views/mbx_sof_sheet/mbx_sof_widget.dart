74aa586b700c9abdba9e6567f2a9bb9b4d1ae94f
Gustaman <>
2024-04-13 00:41:53 +0700

    Sof name colors

--- lib/views/mbx_sof_sheet/mbx_sof_widget.dart
+++ lib/views/mbx_sof_sheet/mbx_sof_widget.dart
@@ -1,3 +1,5 @@
+import 'package:mbankingflutter/utils/all_utils.dart';
+
 import '../../models/mbx_account_model.dart';
 import '../../viewmodels/mbx_format_vm.dart';
 import '../../widgets/all_widgets.dart';
@@ -29,12 +31,18 @@ class MbxSofWidget extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  TextX(
-                    account.name,
-                    color: ColorX.black,
-                    fontSize: 13.0,
-                    fontWeight: FontWeight.w400,
-                    textAlign: TextAlign.start,
+                  ContainerX(
+                    backgroundColor: ColorX.theme.darken(0.03),
+                    padding: EdgeInsets.only(
+                        left: 8.0, top: 2.0, right: 8.0, bottom: 2.0),
+                    cornerRadius: 6.0,
+                    child: TextX(
+                      account.name,
+                      color: ColorX.white,
+                      fontSize: 11.0,
+                      fontWeight: FontWeight.w500,
+                      textAlign: TextAlign.start,
+                    ),
                   ),
                   TextX(
                     account.account,
