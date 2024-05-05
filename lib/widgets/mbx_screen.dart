771eaf523b2265dea2fd52cd034cea1ad368d517
Gustaman <>
2024-05-06 05:39:49 +0700

    MbxScreen back actions

--- lib/widgets/mbx_screen.dart
+++ lib/widgets/mbx_screen.dart
@@ -1,40 +1,61 @@
+import 'package:mbankingflutter/utils/all_utils.dart';
+
 import 'all_widgets.dart';
 
 class MbxScreen extends StatelessWidget {
+  final String title;
+  final GestureTapCallback? backAction;
   final Widget? body;
-
   const MbxScreen({
     Key? key,
+    this.title = '',
+    this.backAction,
     this.body,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ScreenX(
-        lightStatusBar: true,
-        headerView: NavigationBarX(
-          title: '',
-          leftBtn: ImageX(
-            faIcon: FontAwesomeIcons.arrowLeft,
-            width: 20.0,
-            height: 20.0,
-            color: ColorX.white,
-            fit: BoxFit.contain,
-          ),
-          leftAction: () {
-            Get.back();
-          },
-        ),
-        bottomPadding: false,
-        bodyView: Column(
-          children: [
-            TopContainerX(),
-            Expanded(
-                child: ContainerX(
-              backgroundColor: ColorX.white,
-              child: body,
-            )),
-          ],
-        ));
+    return PopScope(
+        canPop: false, // backAction != null,
+        onPopInvoked: (didPop) {
+          LoggerX.log('onPopInvoked: $didPop');
+          if (didPop == false) {
+            if (backAction != null) {
+              backAction!();
+            } else {
+              Get.back();
+            }
+          }
+        },
+        child: ScreenX(
+            lightStatusBar: true,
+            headerView: NavigationBarX(
+              title: title,
+              leftBtn: ImageX(
+                faIcon: FontAwesomeIcons.arrowLeft,
+                width: 20.0,
+                height: 20.0,
+                color: ColorX.white,
+                fit: BoxFit.contain,
+              ),
+              leftAction: () {
+                if (backAction != null) {
+                  backAction!();
+                } else {
+                  Get.back();
+                }
+              },
+            ),
+            bottomPadding: false,
+            bodyView: Column(
+              children: [
+                TopContainerX(),
+                Expanded(
+                    child: ContainerX(
+                  backgroundColor: ColorX.white,
+                  child: body,
+                )),
+              ],
+            )));
   }
 }
