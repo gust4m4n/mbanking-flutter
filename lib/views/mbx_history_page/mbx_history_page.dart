f515556ff4ba54db8ef7757de180b5bc9c9747ce
Gustaman <>
2024-05-07 08:52:26 +0700

    History page mbx screen

--- lib/views/mbx_history_page/mbx_history_page.dart
+++ lib/views/mbx_history_page/mbx_history_page.dart
@@ -9,50 +9,49 @@ class MbxHistoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<MbxHistoryController>(
-        init: MbxHistoryController(),
-        builder: (controller) => MbxScreen(
-              title: 'Riwayat',
-              curvedBody: Expanded(
-                  child: NotificationListener<ScrollNotification>(
-                onNotification: (ScrollNotification scrollInfo) {
-                  if (controller.historyListVM.loading == false &&
-                      scrollInfo.metrics.pixels ==
-                          scrollInfo.metrics.maxScrollExtent) {
-                    controller.nextPage();
-                  }
-                  return true;
-                },
-                child: Scrollbar(
+      init: MbxHistoryController(),
+      builder: (controller) => MbxScreen(
+          title: 'Riwayat',
+          curvedBody: NotificationListener<ScrollNotification>(
+            onNotification: (ScrollNotification scrollInfo) {
+              if (controller.historyListVM.loading == false &&
+                  scrollInfo.metrics.pixels ==
+                      scrollInfo.metrics.maxScrollExtent) {
+                controller.nextPage();
+              }
+              return true;
+            },
+            child: Scrollbar(
+                controller: controller.scrollController,
+                child: ListView.separated(
                     controller: controller.scrollController,
-                    child: ListView.separated(
-                        controller: controller.scrollController,
-                        padding: EdgeInsets.only(
-                            bottom: MediaQuery.of(Get.context!).padding.bottom +
-                                100.0),
-                        physics: ClampingScrollPhysics(),
-                        separatorBuilder: (context, index) {
-                          return Padding(
-                              padding: EdgeInsets.only(left: 50.0),
-                              child: ContainerX(
-                                height: 0.5,
-                                width: double.infinity,
-                                backgroundColor: ColorX.lightGray,
-                              ));
-                        },
-                        itemCount: controller.historyListVM.list.length,
-                        itemBuilder: (context, index) {
-                          final history = controller.historyListVM.list[index];
-                          return InkWellX(
-                              highlightColor: ColorX.theme.withOpacity(0.1),
-                              onClicked: () {
-                                Get.toNamed('/receipt', arguments: {
-                                  'receipt': MbxReceiptModel(),
-                                  'backToHome': false
-                                });
-                              },
-                              child: MbxHistoryCell(history));
-                        })),
-              )),
-            ));
+                    padding: EdgeInsets.only(
+                        bottom:
+                            MediaQuery.of(Get.context!).padding.bottom + 100.0),
+                    physics: ClampingScrollPhysics(),
+                    separatorBuilder: (context, index) {
+                      return Padding(
+                          padding: EdgeInsets.only(left: 50.0),
+                          child: ContainerX(
+                            height: 0.5,
+                            width: double.infinity,
+                            backgroundColor: ColorX.lightGray,
+                          ));
+                    },
+                    itemCount: controller.historyListVM.list.length,
+                    itemBuilder: (context, index) {
+                      final history = controller.historyListVM.list[index];
+                      return InkWellX(
+                          highlightColor: ColorX.theme.withOpacity(0.1),
+                          onClicked: () {
+                            Get.toNamed('/receipt', arguments: {
+                              'receipt': MbxReceiptModel(),
+                              'backToHome': false
+                            });
+                          },
+                          child: MbxHistoryCell(history));
+                    })),
+          )),
+    );
   }
 }
--- lib/views/mbx_notification_page/mbx_notification_page.dart
+++ lib/views/mbx_notification_page/mbx_notification_page.dart
@@ -1,4 +1,5 @@
-import '../../models/mbx_receipt_model.dart';
+import 'package:mbankingflutter/models/mbx_receipt_model.dart';
+
 import '../../widgets/all_widgets.dart';
 import 'mbx_notification_controller.dart';
 import 'mbx_notification_widget.dart';
@@ -9,44 +10,51 @@ class MbxNotificationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<MbxNotificationController>(
-      init: MbxNotificationController(),
-      builder: (controller) => MbxScreen(
-          title: 'Notifikasi',
-          body: NotificationListener<ScrollNotification>(
-            onNotification: (ScrollNotification scrollInfo) {
-              if (controller.notificationListVM.loading == false &&
-                  scrollInfo.metrics.pixels ==
-                      scrollInfo.metrics.maxScrollExtent) {
-                controller.nextPage();
-              }
-              return true;
-            },
-            child: Scrollbar(
-                controller: controller.scrollController,
-                child: ListView.builder(
+        init: MbxNotificationController(),
+        builder: (controller) => ScreenX(
+            lightStatusBar: true,
+            headerView: NavigationBarX(
+              title: 'Notifikasi',
+            ),
+            bottomPadding: false,
+            bodyView: Column(children: [
+              Expanded(
+                  child: NotificationListener<ScrollNotification>(
+                onNotification: (ScrollNotification scrollInfo) {
+                  if (controller.notificationListVM.loading == false &&
+                      scrollInfo.metrics.pixels ==
+                          scrollInfo.metrics.maxScrollExtent) {
+                    controller.nextPage();
+                  }
+                  return true;
+                },
+                child: Scrollbar(
                     controller: controller.scrollController,
-                    padding: EdgeInsets.only(
-                        bottom:
-                            MediaQuery.of(Get.context!).padding.bottom + 100.0),
-                    physics: ClampingScrollPhysics(),
-                    itemCount: controller.notificationListVM.list.length,
-                    itemBuilder: (context, index) {
-                      if (index ==
-                          controller.notificationListVM.list.length - 1) {
-                        controller.nextPage();
-                      }
-                      final history = controller.notificationListVM.list[index];
-                      return InkWellX(
-                          highlightColor: ColorX.theme.withOpacity(0.1),
-                          onClicked: () {
-                            Get.toNamed('/receipt', arguments: {
-                              'receipt': MbxReceiptModel(),
-                              'backToHome': false
-                            });
-                          },
-                          child: MbxNotificationWidget(history));
-                    })),
-          )),
-    );
+                    child: ListView.builder(
+                        controller: controller.scrollController,
+                        padding: EdgeInsets.only(
+                            bottom: MediaQuery.of(Get.context!).padding.bottom +
+                                100.0),
+                        physics: ClampingScrollPhysics(),
+                        itemCount: controller.notificationListVM.list.length,
+                        itemBuilder: (context, index) {
+                          if (index ==
+                              controller.notificationListVM.list.length - 1) {
+                            controller.nextPage();
+                          }
+                          final history =
+                              controller.notificationListVM.list[index];
+                          return InkWellX(
+                              highlightColor: ColorX.theme.withOpacity(0.1),
+                              onClicked: () {
+                                Get.toNamed('/receipt', arguments: {
+                                  'receipt': MbxReceiptModel(),
+                                  'backToHome': false
+                                });
+                              },
+                              child: MbxNotificationWidget(history));
+                        })),
+              )),
+            ])));
   }
 }
