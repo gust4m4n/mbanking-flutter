a6d3db43c9ac50e4211e4cc3828964deaeef8ed8
Gustaman <>
2024-10-21 14:56:19 +0700

    ContainerX updates

--- lib/widgets/container_x.dart
+++ lib/widgets/container_x.dart
@@ -14,6 +14,10 @@ class ContainerX extends StatelessWidget {
   final EdgeInsets padding;
   final EdgeInsets margin;
   final Clip clipBehavior;
+  final Color shadowColor;
+  final double shadowSpreadRadius;
+  final double shadowBlurRadius;
+  final Offset shadowOffset;
   final Widget? child;
 
   const ContainerX({
@@ -31,6 +35,10 @@ class ContainerX extends StatelessWidget {
     this.padding = const EdgeInsets.all(0.0),
     this.margin = const EdgeInsets.all(0.0),
     this.clipBehavior = Clip.antiAlias,
+    this.shadowColor = ColorX.transparent,
+    this.shadowSpreadRadius = 0.0,
+    this.shadowBlurRadius = 0.0,
+    this.shadowOffset = const Offset(0.0, 0.0),
     this.child,
   }) : super(key: key);
 
@@ -43,35 +51,44 @@ class ContainerX extends StatelessWidget {
       margin: margin,
       clipBehavior: clipBehavior,
       decoration: BoxDecoration(
-          color: (gradientColorBegin == null || gradientColorEnd == null)
-              ? backgroundColor
-              : null,
-          gradient: (gradientColorBegin != null && gradientColorEnd != null)
-              ? LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  stops: [
-                      0.1,
-                      1.0
-                    ],
-                  colors: [
-                      gradientColorBegin!,
-                      gradientColorEnd!,
-                    ])
-              : null,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(
-                  topLeftRadius > 0.0 ? topLeftRadius : cornerRadius),
-              topRight: Radius.circular(
-                  topRightRadius > 0.0 ? topLeftRadius : cornerRadius),
-              bottomLeft: Radius.circular(cornerRadius),
-              bottomRight: Radius.circular(cornerRadius)),
-          border: Border.all(
-            width: borderWidth,
-            color: borderWidth == 0.0
-                ? backgroundColor ?? Colors.transparent
-                : borderColor,
-          )),
+        color: (gradientColorBegin == null || gradientColorEnd == null)
+            ? backgroundColor
+            : null,
+        gradient: (gradientColorBegin != null && gradientColorEnd != null)
+            ? LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [
+                    0.1,
+                    1.0
+                  ],
+                colors: [
+                    gradientColorBegin!,
+                    gradientColorEnd!,
+                  ])
+            : null,
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(
+                topLeftRadius > 0.0 ? topLeftRadius : cornerRadius),
+            topRight: Radius.circular(
+                topRightRadius > 0.0 ? topLeftRadius : cornerRadius),
+            bottomLeft: Radius.circular(cornerRadius),
+            bottomRight: Radius.circular(cornerRadius)),
+        border: Border.all(
+          width: borderWidth,
+          color: borderWidth == 0.0
+              ? backgroundColor ?? Colors.transparent
+              : borderColor,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: shadowColor,
+            spreadRadius: shadowSpreadRadius,
+            blurRadius: shadowBlurRadius,
+            offset: shadowOffset,
+          ),
+        ],
+      ),
       child: child,
     );
   }
